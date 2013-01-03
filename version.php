4ccee78ff37dbedd0ac8229df8b62525da3b95c5
BA CodeMonkey <bmbrands@gmail.com>
2013-01-03 11:45:21 +0100

    updated readme.txt

--- version.php
+++ version.php
@@ -5,14 +5,14 @@
  *
  * @package    theme
  * @subpackage bootstrap
- * @copyright  2012 Bas Brands, www.basbrands.nl
+ * @copyright  2013 Bas Brands, www.basbrands.nl
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012121600; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2013010300; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011120500; // Requires this Moodle version
-$plugin->maturity = MATURITY_BETA;             // this version's maturity level
-$plugin->release = '1.0 (Build: 2012121600)';
+$plugin->maturity = MATURITY_STABLE;             // this version's maturity level
+$plugin->release = '2.0 (Build: 2013010300)';
 $plugin->component = 'theme_bootstrap'; // Full name of the plugin (used for diagnostics)
