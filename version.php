7b7e44dae4c36d52a5e73181239d7cd169c09c97
Bas Brands <bas@sonsbeekmedia.nl>
2014-03-17 18:35:03 +0100

    Update version due to new settings available

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014022000;
+$plugin->version   = 2014031700;
 $plugin->requires  = 2013110500;
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release   = 2014022000;
