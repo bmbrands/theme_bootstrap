70fca232dacc49f616799573e6618312fb3cff91
Bas Brands <bas@sonsbeekmedia.nl>
2014-05-14 09:34:23 +0200

    Released on Moodle.org

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2014051200;
+$plugin->version   = 2014051301;
 $plugin->requires  = 2013110500;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = 2014050100;
+$plugin->release   = 2014051300;
 $plugin->component = 'theme_bootstrap';
