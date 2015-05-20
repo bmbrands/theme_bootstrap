e3becc86620c8b15d15302ca9fcd510b95313b58
Bas Brands <bas@sonsbeekmedia.nl>
2015-05-20 19:59:51 +0200

    'more file renderer fixes'

--- renderers.php
+++ renderers.php
@@ -29,6 +29,5 @@ require_once('renderers/admin_renderer.php');
 require_once('renderers/course_renderer.php');
 require_once('renderers/course_format_renderer.php');
 require_once('renderers/course_management.php');
-require_once('renderers/files_renderer.php');
 require_once('renderers/block_settings_renderer.php');
 require_once('renderers/enrol_renderer.php');
