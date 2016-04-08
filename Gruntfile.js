82dfafd20495d0adb346de6d195f6ba05df222d4
Bas Brands <bas@sonsbeekmedia.nl>
2016-04-08 16:50:52 +0200

    attempt to fix travis check

--- Gruntfile.js
+++ Gruntfile.js
@@ -146,24 +146,24 @@ module.exports = function(grunt) { // jshint ignore:line
             }
         },
         autoprefixer: {
-          options: {
-            browsers: [
-              'Android 2.3',
-              'Android >= 4',
-              'Chrome >= 20',
-              'Firefox >= 24', // Firefox 24 is the latest ESR
-              'Explorer >= 8',
-              'iOS >= 6',
-              'Opera >= 12',
-              'Safari >= 6'
-            ]
-          },
-          core: {
             options: {
-              map: false
+                browsers: [
+                    'Android 2.3',
+                    'Android >= 4',
+                    'Chrome >= 20',
+                    'Firefox >= 24', // Firefox 24 is the latest ESR
+                    'Explorer >= 8',
+                    'iOS >= 6',
+                    'Opera >= 12',
+                    'Safari >= 6'
+                ]
             },
-            src: ['style/moodle.css', 'style/moodle-rtl.css', 'style/editor.css'],
-          },
+            core: {
+            options: {
+                map: false
+            },
+                src: ['style/moodle.css', 'style/moodle-rtl.css', 'style/editor.css'],
+            },
         },
         cssmin: {
             options: {
