892cebc8d4945238aa364fc5a785acb6873072bc
Bas Brands <bas@moodle.com>
2019-08-15 08:52:27 +0200

    bump version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2018120300;
+$plugin->version   = 2019081500;
 $plugin->requires  = 2018112800;
 $plugin->component = 'theme_bootstrapbase';
